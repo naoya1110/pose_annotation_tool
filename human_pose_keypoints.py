454780da5a6708402cd70a094c0f99365c7af8fd
naoya1110 <naoya.1110@gmail.com>
2024-05-25 22:12:04 +0900

    updated

---
+++ human_pose_keypoints.py
@@ -0,0 +1,19 @@
+mscoco = {
+    1:"nose",
+    2:"left_eye",
+    3:"right_eye",
+    4:"left_ear",
+    5:"right_ear",
+    6:"left_shoulder",
+    7:"right_shoulder",
+    8:"left_elbow",
+    9:"right_elbow",
+    10:"left_wrist",
+    11:"right_wrist",
+    12:"left_hip",
+    13:"right_hip",
+    14:"left_knee",
+    15:"right_knee",
+    16:"left_ankle",
+    17:"right_ankle"
+}
